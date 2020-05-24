ba381cfd367bccee68b1e5b4a3e2a087d7298c8d
Rayson Koh <koh_rayson@yahoo.com.sg>
2020-05-25 00:22:22 +0800

    Updated eslintrc and profile_pic

--- src/components/MenuBar/MenuBar.js
+++ src/components/MenuBar/MenuBar.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import { Avatar } from '@material-ui/core';
-import pic from './casual_pic_1.jpg';
-import './MenuBar.css';
+import React from "react";
+import {Avatar} from "@material-ui/core";
+import pic from "./profile_pic.jpg";
+import "./MenuBar.css";
 
 const MenuBar = () => {
   return (
-    <div className='MenuBar'>
-      <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-        <Avatar alt='Rayson Koh' style={{ width: '9em', height: '9em' }} src={pic} />
+    <div className="MB">
+      <div className="MB-Avatar-Container">
+        <Avatar alt="Rayson Koh" className="MB-Avatar" src={pic} />
       </div>
       THIS IS THE MENUBAR!
     </div>
-  )
-}
+  );
+};
 
 export default MenuBar;
+
