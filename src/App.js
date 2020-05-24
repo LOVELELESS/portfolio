73ea013cbb63900ec6ce3cd22832ce015b504cca
Rayson Koh <koh_rayson@yahoo.com.sg>
2020-05-25 01:44:07 +0800

    Created MenuButton component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
-import Header from "./components/MenuBar/MenuBar";
+import Header from "./components/Header/Header";
+import MenuButton from "./components/MenuButton/MenuButton";
+import WorkIcon from "@material-ui/icons/Work";
+import "./App.css";
 
 // eslint-disable-next-line require-jsdoc
 function App() {
   return (
     <div>
       <Header />
+      <MenuButton renderIcon={() => <WorkIcon />} text="button" />
     </div>
   );
 }
---
+++ src/components/MenuButton/MenuButton.js
@@ -0,0 +1,31 @@
+import React from "react";
+import {IconButton} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
+
+// eslint-disable-next-line require-jsdoc
+const MenuButton = (props) => {
+  /**
+   * The reason I'm doing this weird makeStyles thing is to be able to display
+   * text below IconButton. I'm not sure why I can't just in-line CSS the
+   * flex-direction column...
+   */
+  const useStyles = makeStyles((theme) => ({
+    iconButton: {
+      display: "flex",
+      flexDirection: "column",
+    },
+  }));
+
+  const classes = useStyles();
+
+  return (
+    <div>
+      <IconButton classes={{label: classes.iconButton}}>
+        {props.renderIcon()}
+        <div>{props.text}</div>
+      </IconButton>
+    </div>
+  );
+};
+
+export default MenuButton;
