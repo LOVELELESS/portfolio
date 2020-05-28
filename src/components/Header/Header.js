d2194e50e1f5c09b908aa7ace67e1434274741e3
Rayson Koh <koh_rayson@yahoo.com.sg>
2020-05-29 04:36:19 +0800

    Added media queries for Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link, Avatar, Typography} from "@material-ui/core";
+import { Link, Avatar, Typography } from "@material-ui/core";
 import pic from "./profile_pic.jpg";
 import "./Header.css";
 
@@ -9,26 +9,65 @@ const Header = () => {
       <div className="Header-Avatar-Container">
         <Avatar alt="Rayson Koh" className="Header-Avatar" src={pic} />
       </div>
-      <Typography variant="h2">Hello there! I am Rayson.</Typography>
-      <Typography variant="h2" gutterBottom>I love working on impactful software engineering projects at scale.</Typography>
+      <Typography variant="h2">Hello world! I am Rayson.</Typography>
+      <Typography variant="h2" gutterBottom>
+        I love working on interesting and challenging software engineering
+        projects.
+      </Typography>
       <div>
-        <Typography variant="h6" color="secondary" display="inline" gutterBottom>Currently: </Typography>
-        <Typography variant="h6" display="inline">Year 1 Computer Science Undergraduate @{" "}
-          <Link target="_blank" rel="noopener noreferrer" href="https://www.comp.nus.edu.sg/programmes/ug/cs/" color="textSecondary" >National University of Singapore</Link>
+        <Typography
+          variant="h6"
+          color="secondary"
+          display="inline"
+          gutterBottom
+        >
+          Currently:{" "}
+        </Typography>
+        <Typography variant="h6" display="inline">
+          Year 1 Computer Science Undergraduate @{" "}
+          <Link
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.comp.nus.edu.sg/programmes/ug/cs/"
+            color="textSecondary"
+          >
+            National University of Singapore
+          </Link>
         </Typography>
       </div>
-      <div className="Header-Contacts">
-        <Typography variant="h6" display="inline" color="secondary">My Contacts: </Typography>
-        <a target="_blank" rel="noopener noreferrer" href="https://github.com/LOVELELESS">
-          <img className="Footer-Icon" src="https://img.icons8.com/material-sharp/36/000000/github.png"/>
-        </a>
-        <a target="_blank" rel="noopener noreferrer" href="https://sg.linkedin.com/in/raysonkoh">
-          <img className="Footer-Icon"src="https://img.icons8.com/color/36/000000/linkedin.png"/>
-        </a>
-        <a target="_blank" rel="noopener noreferrer" href="mailto:koh_rayson@yahoo.com.sg">
-          <img className="Footer-Icon" src="https://img.icons8.com/nolan/36/email.png"/>
-        </a>
-      </div>
+      <Typography variant="h6" display="inline" color="secondary">
+        My Contacts:{" "}
+      </Typography>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/LOVELELESS"
+      >
+        <img
+          className="Footer-Icon"
+          src="https://img.icons8.com/material-sharp/36/000000/github.png"
+        />
+      </a>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://sg.linkedin.com/in/raysonkoh"
+      >
+        <img
+          className="Footer-Icon"
+          src="https://img.icons8.com/color/36/000000/linkedin.png"
+        />
+      </a>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="mailto:koh_rayson@yahoo.com.sg"
+      >
+        <img
+          className="Footer-Icon"
+          src="https://img.icons8.com/nolan/36/email.png"
+        />
+      </a>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,30 +3,62 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
-import {createMuiTheme, responsiveFontSizes, ThemeProvider} from "@material-ui/core/styles";
+import {
+  createMuiTheme,
+  responsiveFontSizes,
+  MuiThemeProvider,
+} from "@material-ui/core/styles";
 
-const theme = responsiveFontSizes(
-    createMuiTheme({
-      typography: {
-        fontFamily: [
-          "Montserrat",
-          "sans-serif",
-        ].join(","),
-        fontWeightLight: 300,
-        fontWeightRegular: 500,
-        fontWeightMedium: 700,
-        fontWeightBold: 900,
+let theme = createMuiTheme({
+  typography: {
+    fontFamily: ["Montserrat", "sans-serif"].join(","),
+    fontWeightLight: 300,
+    fontWeightRegular: 500,
+    fontWeightMedium: 700,
+    fontWeightBold: 900,
+    /*
+    h1: {
+      "@media only screen and (max-width: 768px)": {
+        fontSize: "4rem",
       },
-    }),
-);
+    },
+    h2: {
+      "@media only screen and (max-width: 768px)": {
+        fontSize: "2.5rem",
+      },
+    },
+    h3: {
+      "@media only screen and (max-width: 768px)": {
+        fontSize: "2rem",
+      },
+    },
+    h4: {
+      "@media only screen and (max-width: 768px)": {
+        fontSize: "1.4rem",
+      },
+    },
+    h5: {
+      "@media only screen and (max-width: 768px)": {
+        fontSize: "1rem",
+      },
+    },
+    h6: {
+      "@media only screen and (max-width: 768px)": {
+        fontSize: "0.83rem",
+      },
+    },
+    */
+  },
+});
+theme = responsiveFontSizes(theme);
 
 ReactDOM.render(
-    <React.StrictMode>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </React.StrictMode>,
-    document.getElementById("root"),
+  <React.StrictMode>
+    <MuiThemeProvider theme={theme}>
+      <App />
+    </MuiThemeProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
