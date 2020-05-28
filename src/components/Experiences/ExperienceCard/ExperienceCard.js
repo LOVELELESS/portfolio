2fce4df9bf69a6a3ecac02212c34fbcd19c3abfe
Rayson Koh <koh_rayson@yahoo.com.sg>
2020-05-29 05:56:07 +0800

    Added media queries for Experience component

--- src/components/Experiences/ExperienceCard/ExperienceCard.js
+++ src/components/Experiences/ExperienceCard/ExperienceCard.js
@@ -1,5 +1,12 @@
 import React from "react";
-import {Card, Button, CardContent, CardActions, CardMedia, Typography} from "@material-ui/core";
+import {
+  Card,
+  Button,
+  CardContent,
+  CardActions,
+  CardMedia,
+  Typography,
+} from "@material-ui/core";
 import TimePeriod from "../TimePeriod/TimePeriod";
 import "./ExperienceCard.css";
 
@@ -8,33 +15,64 @@ const ExperienceCard = (props) => {
   return (
     <Card className="ExperienceCard" variant="outlined">
       <CardContent className="ExperienceCard-Title-Container">
-        <a className="ExperienceCard-Title-Media-Container" href={props.link} target="_blank" rel="noopener noreferrer">
-          <CardMedia title="media" className="ExperienceCard-Title-Media" title={props.alt} image={props.image} />
+        <a
+          className="ExperienceCard-Title-Media-Container"
+          href={props.link}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <CardMedia
+            title="media"
+            className="ExperienceCard-Title-Media"
+            title={props.alt}
+            image={props.image}
+          />
         </a>
         <div>
-          <div style={{display: "flex", alignItems: "center"}}>
-            <Typography variant="h6" color="textPrimary">{props.text.title}</Typography>
+          <div className="ExperienceCard-Title-Text-Container">
+            <Typography
+              variant="h6"
+              color="textPrimary"
+              style={{ marginRight: "1em" }}
+            >
+              {props.text.title}
+            </Typography>
             <TimePeriod date={props.date} />
           </div>
-          <Typography variant="subtitle1" color="textPrimary">{props.text.title2}</Typography>
+          <Typography variant="subtitle1" color="textPrimary">
+            {props.text.title2}
+          </Typography>
         </div>
       </CardContent>
       <CardContent>
-        {props.text.description &&
+        {props.text.description && (
           <Typography variant="body1">{props.text.description}</Typography>
-        }
-        {props.text.list &&
-            <ul>
-              {props.text.list.map((listItems) => <li>
-                <Typography variant="body2" color="textSecondary">{listItems}</Typography>
-              </li>)}
-            </ul>
-        }
+        )}
+        {props.text.list && (
+          <ul>
+            {props.text.list.map((listItems) => (
+              <li>
+                <Typography variant="body2" color="textSecondary">
+                  {listItems}
+                </Typography>
+              </li>
+            ))}
+          </ul>
+        )}
         <div className="ExperienceCard-LetterOfRec">
           {props.letterOfRec ? (
-            <a style={{textDecoration: "none"}} href={props.letterOfRec} download={props.letterOfRecName}>
-              <Button variant="outlined" color="secondary">Letter of Recommendation</Button>
-            </a>) : <div />}
+            <a
+              style={{ textDecoration: "none" }}
+              href={props.letterOfRec}
+              download={props.letterOfRecName}
+            >
+              <Button variant="outlined" color="secondary">
+                Letter of Recommendation
+              </Button>
+            </a>
+          ) : (
+            <div />
+          )}
         </div>
       </CardContent>
     </Card>
--- src/components/Experiences/Experiences.js
+++ src/components/Experiences/Experiences.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Typography, Divider} from "@material-ui/core";
+import { Typography, Divider } from "@material-ui/core";
 import ExperienceCard from "./ExperienceCard/ExperienceCard";
 import govtech from "./govtech.png";
 import evantage from "./evantage.png";
@@ -10,7 +10,9 @@ import "./Experiences.css";
 const Experiences = () => {
   return (
     <div className="Experiences">
-      <Typography variant="h4" style={{color: "white", textAlign: "center"}}>Internships</Typography>
+      <Typography variant="h4" style={{ color: "white", textAlign: "center" }}>
+        Internships
+      </Typography>
       <ExperienceCard
         image={govtech}
         alt="GovTech"
@@ -23,9 +25,9 @@ const Experiences = () => {
           title: "Government Technology Agency of Singapore (GovTech)",
           title2: "Software Engineering Internship (3 Months)",
           // maybe change this to a render function to be able to render out Links and stuff like that?
-          description: "Attached to the Government Digital Services team. Currently working on the Moments of Life mobile app.",
-          list: [
-          ],
+          description:
+            "Attached to the Government Digital Services team. Currently working on the Moments of Life mobile app.",
+          list: [],
         }}
       />
       <ExperienceCard
@@ -40,16 +42,20 @@ const Experiences = () => {
           title: "Evantage Technology Pte Ltd",
           title2: "Engineering Internship (6 Months)",
           // maybe change this to a render function to be able to render out Links and stuff like that?
-          description: "Worked with the Engineering team to resolve technical tickets through onsite client visits and remote sessions.",
+          description:
+            "Worked with the Engineering team to resolve technical tickets through onsite client visits and remote sessions.",
           list: [
             "Migrated on-premise fileserver and knowledge-base to the cloud.",
             "Workstation imaging with sysprep for quick deployment.",
             "Developed a prototype digital receptionist based on the alexa echo device using Alexa Skills, AWS Lambda and EC2.",
-          ]}}
+          ],
+        }}
         letterOfRec={evantageTestimonial}
         letterOfRecName="Rayson-Intern-Testimonial-2019.pdf"
       />
-      <Typography variant="h4" style={{color: "white", textAlign: "center"}}>Teaching Experience</Typography>
+      <Typography variant="h4" style={{ color: "white", textAlign: "center" }}>
+        Teaching Experience
+      </Typography>
       <ExperienceCard
         image={soc}
         alt="School of Computing"
@@ -62,7 +68,8 @@ const Experiences = () => {
           title: "National University of Singapore, School of Computing",
           title2: "CS2030 Lab Tutor (5 Months)",
           // maybe change this to a render function to be able to render out Links and stuff like that?
-          description: "Was a lab tutor in a core Computer Science module which teaches Object-Oriented and Funtional Programming.",
+          description:
+            "Was a lab tutor in a core Computer Science module which teaches Object-Oriented and Funtional Programming.",
           list: [
             "Conducted lab briefings for each lab session (total of 10 sessions).",
             "Guide and assess students' lab submissions.",
