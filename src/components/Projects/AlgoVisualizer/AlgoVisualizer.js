66eca204d250ed51993ab31291f8ce1bac61855e
Rayson Koh <koh_rayson@yahoo.com.sg>
2020-05-29 06:32:35 +0800

    Added media queries to AlgoVisualizer

--- src/components/Projects/AlgoVisualizer/AlgoVisualizer.js
+++ src/components/Projects/AlgoVisualizer/AlgoVisualizer.js
@@ -1,6 +1,14 @@
 import React from "react";
-import {withStyles} from "@material-ui/core/styles";
-import {Tooltip, Button, Card, CardContent, CardHeader, Typography, CardActions} from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
+import {
+  Tooltip,
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  Typography,
+  CardActions,
+} from "@material-ui/core";
 import "./AlgoVisualizer.css";
 
 const AlgoVisualizer = () => {
@@ -12,16 +20,29 @@ const AlgoVisualizer = () => {
 
   return (
     <Card>
-      <CardHeader title="CS2040S Algorithm Visualizer" titleTypographyProps={{color: "primary", variant: "h5"}}/>
-      <Typography color="textSecondary" variant="subtitle1">Visualizes the different algorithms taught in CS2040S, the Data Structures and Algorithms course in NUS</Typography>
+      <CardHeader
+        title="CS2040S Algorithm Visualizer"
+        titleTypographyProps={{ color: "primary", variant: "h5" }}
+      />
+      <Typography color="textSecondary" variant="subtitle1">
+        Visualizes the different algorithms taught in CS2040S, the Data
+        Structures and Algorithms course in NUS
+      </Typography>
       <CardContent>
-        <a href="http://cs2040s-algo-visualizer.raysonkoh.tech" target="_blank" rel="noopener noreferrer">
-          <div className="AlgoVisualizer-Media">
-          </div>
+        <a
+          href="http://cs2040s-algo-visualizer.raysonkoh.tech"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="AlgoVisualizer-Media"></div>
         </a>
         <div className="AlgoVisualizer-Text">
-          <Typography variant="body1" color="textPrimary">A responsive visualizer web-app built with React and Redux.</Typography>
-          <Typography variant="body1" color="textPrimary">Some examples of the types of algorithms implemented: </Typography>
+          <Typography variant="body1" color="textPrimary">
+            A responsive visualizer web-app built with React and Redux.
+          </Typography>
+          <Typography variant="body1" color="textPrimary">
+            Some examples of the types of algorithms implemented:{" "}
+          </Typography>
           <Typography variant="body1" color="textSecondary">
             <ul>
               <li>Graph Algorithms</li>
@@ -32,23 +53,54 @@ const AlgoVisualizer = () => {
           </Typography>
         </div>
         <CardActions className="AlgoVisualizer-ActionArea">
-          <a style={{textDecoration: "none"}} href="https://github.com/LOVELELESS/cs2040s-algo-visualizer" target="_blank" rel="noopener noreferrer">
-            <Button variant="outlined" className="AlgoVisualizer-Button" color="secondary">Github Repo</Button>
+          <a
+            style={{ textDecoration: "none" }}
+            href="https://github.com/LOVELELESS/cs2040s-algo-visualizer"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Button
+              variant="outlined"
+              className="AlgoVisualizer-Button"
+              color="secondary"
+            >
+              Github Repo
+            </Button>
           </a>
-          <a style={{textDecoration: "none"}} href="http://cs2040s-algo-visualizer.raysonkoh.tech" target="_blank" rel="noopener noreferrer">
-            <Button variant="outlined" className="AlgoVisualizer-Button" color="secondary">Application</Button>
+          <a
+            style={{ textDecoration: "none" }}
+            href="http://cs2040s-algo-visualizer.raysonkoh.tech"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Button
+              variant="outlined"
+              className="AlgoVisualizer-Button"
+              color="secondary"
+            >
+              Application
+            </Button>
           </a>
-          <span className="AlgoVisualizer-Icons">
-            <CustomTooltip title="React">
-              <img className="AlgoVisualizer-Icon" src="https://img.icons8.com/officel/48/000000/react.png"/>
+          <div className="AlgoVisualizer-Icons">
+            <CustomTooltip enterTouchDelay={50} title="React">
+              <img
+                className="AlgoVisualizer-Icon"
+                src="https://img.icons8.com/officel/48/000000/react.png"
+              />
             </CustomTooltip>
-            <CustomTooltip title="Redux">
-              <img className="AlgoVisualizer-Icon" src="https://img.icons8.com/color/48/000000/redux.png"/>
+            <CustomTooltip enterTouchDelay={50} title="Redux">
+              <img
+                className="AlgoVisualizer-Icon"
+                src="https://img.icons8.com/color/48/000000/redux.png"
+              />
             </CustomTooltip>
-            <CustomTooltip title="Javascript">
-              <img className="AlgoVisualizer-Icon" src="https://img.icons8.com/color/48/000000/javascript.png"/>
+            <CustomTooltip enterTouchDelay={50} title="Javascript">
+              <img
+                className="AlgoVisualizer-Icon"
+                src="https://img.icons8.com/color/48/000000/javascript.png"
+              />
             </CustomTooltip>
-          </span>
+          </div>
         </CardActions>
       </CardContent>
     </Card>
