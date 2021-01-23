c2536738cb107adb899b4ea4ca3b6cb66c913727
raysonkoh <raysonkoh@users.noreply.github.com>
2021-01-23 21:48:46 +0000

    deploy: 4609cfb4f4f61450f222b263ba6bf83e6c9bad60

---
+++ precache-manifest.a4252ea9546d1df96d417dc201196924.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa81f9772d50c893c814132a6a14a9f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff92846229c7c00a927d",
+    "url": "/static/css/main.22258af3.chunk.css"
+  },
+  {
+    "revision": "eefff87aaf560b7dd6a6",
+    "url": "/static/js/2.f887a012.chunk.js"
+  },
+  {
+    "revision": "fde5ff8f4122a466397a13ef98d4f94d",
+    "url": "/static/js/2.f887a012.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff92846229c7c00a927d",
+    "url": "/static/js/main.4ea1d34b.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "62907aa55196a0e949900cb3571439f7",
+    "url": "/static/media/AlgoVisualizer_better.62907aa5.jpg"
+  },
+  {
+    "revision": "ad8e09fd280f1ca416e4b9218b653ad9",
+    "url": "/static/media/Evantage-Testimonial.ad8e09fd.pdf"
+  },
+  {
+    "revision": "5859876b2a6de4e8fcda922e84f5c914",
+    "url": "/static/media/Govtech-Testimonial.5859876b.pdf"
+  },
+  {
+    "revision": "5bf734826e40f8375109f9bb52416ef1",
+    "url": "/static/media/Rayson_CVE-2019-0708-REDACTED.5bf73482.pdf"
+  },
+  {
+    "revision": "8f098251e15c9e65a1820c3d79364b7c",
+    "url": "/static/media/Signin.8f098251.png"
+  },
+  {
+    "revision": "67c44241073bf8c61ff87a86b980f693",
+    "url": "/static/media/app-universal-release.67c44241.apk"
+  },
+  {
+    "revision": "e53a5f6741d9086a6c7c6ebe8af1ebff",
+    "url": "/static/media/bluekeep.e53a5f67.jpeg"
+  },
+  {
+    "revision": "a0be003380039d61dcffb63dd7c833dd",
+    "url": "/static/media/ccna_cert.a0be0033.pdf"
+  },
+  {
+    "revision": "f463928cd6f9370fadaf5ee881c6e6af",
+    "url": "/static/media/ccna_routerswitching_large.f463928c.jpg"
+  },
+  {
+    "revision": "d126985394637f89bee25c67acce7a77",
+    "url": "/static/media/evantage.d1269853.png"
+  },
+  {
+    "revision": "a18ddbdbda62acc003562a391ff3eb80",
+    "url": "/static/media/govtech.a18ddbdb.png"
+  },
+  {
+    "revision": "9ba8c9de9012b716b3e6a3a4c10ee827",
+    "url": "/static/media/profile_pic.9ba8c9de.jpg"
+  },
+  {
+    "revision": "f31ddfae5566a78fae1a519e11a1ffff",
+    "url": "/static/media/soc.f31ddfae.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/raysonkoh.com/precache-manifest.45c0cae4338255b0b5c265aa182a294f.js"
+  "/precache-manifest.a4252ea9546d1df96d417dc201196924.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/raysonkoh.com/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
