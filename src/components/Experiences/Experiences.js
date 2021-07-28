6516a6419e555590227c16f9697b366820882384
raysonkoh <rayson.rk.koh@gmail.com>
2021-07-28 17:13:03 +0800

    Update Shopee experience

--- src/components/Experiences/Experiences.js
+++ src/components/Experiences/Experiences.js
@@ -5,8 +5,7 @@ import govtech from './govtech.png';
 import evantage from './evantage.png';
 import soc from './soc.jpg';
 import shopee from './shopee.png';
-import evantageTestimonial from './Evantage-Testimonial.pdf';
-import govtechTestimonial from './Govtech-Testimonial.pdf';
+import blank from './blank.pdf';
 import './Experiences.css';
 
 const Experiences = () => {
@@ -28,8 +27,15 @@ const Experiences = () => {
           title2: 'Software Engineer Intern (3 Months)',
           // maybe change this to a render function to be able to render out Links and stuff like that?
           description:
-            'Currently working in the Shared Services team.',
+            'Shared Services, Item Price Stock Team',
+          list: [
+            'Executed the migration of Kafka clusters over to Kafka clusters with authentication, leading to better security',
+            'Implemented Memcached caching for invalid Item Stock requests, thus greatly improving service performance',
+            'Configured liveness and readiness tests for Item pipelines using Go, thereby improving its uptime in production',
+          ],
         }}
+        testimonial={blank}
+        testimonialName="Recommendation letter for Rayson Koh.pdf"
       />
       <ExperienceCard
         image={govtech}
@@ -44,14 +50,14 @@ const Experiences = () => {
           title2: 'Software Engineer Intern (3 Months)',
           // maybe change this to a render function to be able to render out Links and stuff like that?
           description:
-            'Worked on the Moments of Life (LifeSG) mobile app which integrates government services.',
+            'Moments of Life (LifeSG) Team',
           list: [
-            'Developed a recommendation backend service using Koa and Elasticsearch',
-            'Implemented UX mockups using TypeScript and React Native',
-            'Set up feature toggles using Redux and ConfigCat to allow seamless rollout of app features to production',
+            'Developed a recommendation service using Koa and Elasticsearch in order to improve user engagement',
+            'Set up feature toggle functionality using Redux for fast deployment of new features to production',
+            'Implemented wireframes and mockups using Typescript and React Native',
           ],
         }}
-        testimonial={govtechTestimonial}
+        testimonial={blank}
         testimonialName="Recommendation letter for Rayson Koh.pdf"
       />
       <ExperienceCard
@@ -67,15 +73,14 @@ const Experiences = () => {
           title2: 'IT Engineer Intern (6 Months)',
           // maybe change this to a render function to be able to render out Links and stuff like that?
           description:
-            'Worked with the Engineering team to resolve technical issues through onsite client visits and remote sessions.',
+            'Engineering Team',
           list: [
-            'Migrated on-premise fileserver and knowledge-base to the cloud',
+            'Executed the cloud migration of the on-premise file server and knowledge base, hence resulting in better reliability',
             'Troubleshooted on-site network issues',
-            'Deployed workstations at scale using sysprep imaging',
           ],
         }}
-        testimonial={evantageTestimonial}
-        testimonialName="Rayson-Intern-Testimonial-2019.pdf"
+        testimonial={blank}
+        testimonialName="Recommendation letter for Rayson Koh.pdf"
       />
       <Typography variant="h4" style={{ color: 'white', textAlign: 'center' }}>
         Teaching Experience
@@ -93,7 +98,7 @@ const Experiences = () => {
           title2: 'CS2030 Lab Tutor (5 Months)',
           // maybe change this to a render function to be able to render out Links and stuff like that?
           description:
-            'Worked as a lab tutor for a core Computer Science module which teaches Object-Oriented and Funtional Programming in Java.',
+            'Lab Tutor for a core Computer Science module which teaches Object-Oriented and Funtional Programming in Java.',
           list: [
             'Conducted lab briefings for each lab session (total of 10 sessions)',
             'Guide and assess students\' lab submissions',
