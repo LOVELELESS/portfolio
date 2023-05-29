dcf3874dacca80f6618bc72ab1f2a170b4250678
Rayson Koh <rayson.rk.koh@gmail.com>
2023-05-29 01:06:27 +0000

    Update CAP

--- src/components/Academics/AtAGlance/AtAGlance.js
+++ src/components/Academics/AtAGlance/AtAGlance.js
@@ -52,7 +52,7 @@ const AtAGlance = () => {
                 with a Minor in Mathematics
               </Typography>
               <Typography variant="subtitle1" color="secondary" gutterBottom>
-                GPA: 4.83 / 5.00
+                GPA: 4.84 / 5.00
               </Typography>
               <Typography
                 variant="subtitle2"
